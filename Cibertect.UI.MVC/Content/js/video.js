1af0919edc495f8aa5b43b0f25f46b0bc1c512c7
Jorge <jsandoval@pucp.edu.pe>
2019-01-07 11:12:05 -0500

    Initial commit

---
+++ Cibertect.UI.MVC/Content/js/video.js
@@ -0,0 +1,16 @@
+﻿
+(function (cibertec) {
+
+    cibertec.video = {
+
+        play: function (videoId) {
+            var videoElement = document.getElementById(videoId);
+            videoElement.play();
+        },
+
+        pause: function (videoId) {
+            var videoElement = document.getElementById(videoId);
+            videoElement.pause();
+        }
+    }
+})(window.cibertec = window.cibertec || {});
